fe468f28b0717786aff1b3bd838227bc0ce2d025
로인 <151013952+bboroin@users.noreply.github.com>
2025-10-11 18:57:36 +0900

    feat: Header 초기 레이아웃 구현 #7

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import searchIcon from "../../assets/icons/search.svg";
+import avatarIcon from "../../assets/icons/avatar.svg";
 
 const Header = () => {
   return (
-    <div>
-      <h2>Header</h2>
+    <div className="header">
+      <div className="logo">
+        <img src="/logo.png" alt="로고" />
+      </div>
+      <div className="search-container">
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search Movie"
+        />
+        <button className="search-btn">
+          <img src={searchIcon} alt="검색 버튼" />
+        </button>
+      </div>
+      <button className="user-btn">
+        <img src={avatarIcon} alt="사용자 프로필" />
+      </button>
     </div>
   );
 };
