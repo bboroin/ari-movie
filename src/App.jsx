5304b7ba76ce8ea2a4f28e1040d8545fad4ed814
로인 <151013952+bboroin@users.noreply.github.com>
2025-10-19 20:08:01 +0900

    feat: 검색 페이지 라우팅(/search) 추가 #21

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import Home from "./pages/Home";
+import Search from "./pages/Search";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <main>
-        <Home />
-      </main>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
