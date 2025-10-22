c9f8b2204f454077d9179f1c52149b8166ab9c73
로인 <151013952+bboroin@users.noreply.github.com>
2025-10-23 02:20:38 +0900

    chore: useParams로 id 표시하여 이동 경로 확인용 텍스트 출력 #27

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const MovieDetail = () => {
+  const { id } = useParams();
+
   return (
     <div>
       <h1>Detail 페이지</h1>
+      <p>id: {id}</p>
     </div>
   );
 };
